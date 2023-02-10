e576fc647adcffc716d96d0215df711301b4e027
Shadowa <85016881+Shadowa123Baran@users.noreply.github.com>
2023-02-10 23:38:44 +0400

    Add files via upload

---
+++ task3/dist/profile.js
@@ -0,0 +1,7 @@
+"use strict";
+
+function printprofile(profile) {
+  var name = profile.name,
+    company = profile.company;
+  console.log("".concat(name, " from ").concat(company, ", Ohio, USA"));
+}
